436b15eebc774bc5cac41a3749318ab6e6e8b5b4
Beastie-UH <youri.mout@gmail.com>
2013-05-22 17:55:34 +0200

    update

--- barconfig.h
+++ barconfig.h
@@ -11,11 +11,11 @@
 /* Default bar position, overwritten by '-b' switch */
 #define BAR_BOTTOM 0
 /* The fonts used for the bar, comma separated. Only the first 2 will be used. */
-//#define BAR_FONT "fixed", "fixed"
-#define BAR_FONT "-beastie-gohufontmod11-medium-r-normal--11-80-100-100-c-60-iso8859-1", "fixed"
+#define BAR_FONT "-artwiz-cure-medium-r-normal--11-110-75-75-p-90-iso8859-1", "fixed"
 /* Some fonts don't set the right width for some chars, pheex it */
 #define BAR_FONT_FALLBACK_WIDTH 6
 /* Color palette */
+
 #define COLOR0	0x080808
 #define COLOR1	0x808080
 #define COLOR2	0x1a1a1a
