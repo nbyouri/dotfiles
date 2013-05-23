1edc3e87ebebee57fb73f82eceadcd673da7a4f5
Beastie-UH <youri.mout@gmail.com>
2013-05-23 13:26:20 +0200

    okay

--- barconfig.h
+++ barconfig.h
@@ -5,11 +5,11 @@
 /* Offset from the left. Set to 0 to have no effect */
 #define BAR_OFFSET 0
 /* Choose between an underline or an overline */
-#define BAR_UNDERLINE 1
+#define BAR_UNDERLINE 1 
 /* The thickness of the underline (in pixels). Set to 0 to disable. */
-#define BAR_UNDERLINE_HEIGHT 2
+#define BAR_UNDERLINE_HEIGHT 1 
 /* Default bar position, overwritten by '-b' switch */
-#define BAR_BOTTOM 0
+#define BAR_BOTTOM 0 
 /* The fonts used for the bar, comma separated. Only the first 2 will be used. */
 #define BAR_FONT "-artwiz-cure-medium-r-normal--11-110-75-75-p-90-iso8859-1", "fixed"
 /* Some fonts don't set the right width for some chars, pheex it */
