614bd262d8a9291663c49bcb73eada7d5f0a4f18
yrmt <youri.mout@gmail.com>
2013-12-20 18:10:46 +0100

    Added a few things

--- info/info.c
+++ info/info.c
@@ -6,12 +6,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <pwd.h>
 #include <sys/sysctl.h>
 #include <sys/statvfs.h>
 #include <sqlite3.h>
 
-#define NOR  "\x1B[0m"
-#define RED  "\x1B[31m"
+#define C0  "\x1B[0m"    //Reset
+#define C1  "\x1B[0;32m" //Green
+#define C2  "\x1B[0;33m" //Yellow
+#define C3  "\x1B[1;31m" //RED
+#define C4  "\x1B[0;31m" //Red
+#define C5  "\x1B[0;35m" //Purple
+#define C6  "\x1B[0;36m" //Blue
+#define RED C3
+#define NOR C0
+
+bool print_with_apple = false;
 
 static const struct {
     const char *ctls;
@@ -23,34 +35,68 @@ static const struct {
     { "kern.ostype", "OS" },
     { "kern.osrelease", "Kernel" },
     { "kern.hostname", "Hostname" },
-    { "SHELL", "Shell" },
-    { "TERM", "Terminal" },
 };
-void sysctls(void) {
+
+static void sysctls(int i);
+static void envs(int i);
+static void disk(void);
+static void pkg(void);
+static void print_uptime(time_t *nowp);
+
+static void print_apple(void) {
+    time_t now;
+    (void)time(&now);
+    printf(C1"                :++++.        ");envs(3);
+    printf(C1"               /+++/.         ");sysctls(0);
+    printf(C1"       .:-::- .+/:-``.::-     ");sysctls(3); 
+    printf(C2"    .:/++++++/::::/++++++/:`  ");sysctls(4);
+    printf(C2"  .:///////////////////////:` ");sysctls(5);
+    printf(C2"  ////////////////////////`   ");sysctls(2);
+    printf(C3" -+++++++++++++++++++++++`    ");envs(2);
+    printf(C3" /++++++++++++++++++++++/     ");envs(1);
+    printf(C4" /sssssssssssssssssssssss.    ");sysctls(1);
+    printf(C4" :ssssssssssssssssssssssss-   ");disk();
+    printf(C5"  osssssssssssssssssssssssso/ ");pkg();
+    printf(C5"  `syyyyyyyyyyyyyyyyyyyyyyyy+ ");print_uptime(&now);
+    printf(C5"   `ossssssssssssssssssssss/  \n");
+    printf(C6"     :ooooooooooooooooooo+.   \n");
+    printf(C6"      `:+oo+/:-..-:/+o+/-     \n");
+}
+void help(void) {
+    printf("Mac OS X Info script by yrmt dec. 2013\n"
+            "\t-a shows a colored apple\n"
+            "\t-h shows this help msg\n");
+    exit(1);
+}
+static void sysctls(int i) {
     size_t len;
     uint64_t value64;
-    for(int i=0;i<6;i++) {
-        if(i == 2) {
-            sysctlbyname(values[i].ctls, &value64, &len, NULL, 0);
-            printf(RED"%-10s: "NOR"%.f MB\n", values[i].names, value64/1e+06);
-        } else {
-            sysctlbyname(values[i].ctls, NULL, &len, NULL, 0);
-            char *type = malloc(len);
-            sysctlbyname(values[i].ctls, type, &len, NULL, 0);
-            printf(RED"%-10s: "NOR"%s\n", values[i].names, type);
-            free(type);
-        }
-    }
-}
-void envs(void) {
-    char *type;
-    char *dummy;
-    for(int i=6;i<8;i++) {
-        type = getenv(values[i].ctls);
+    if(i == 2) {
+        sysctlbyname(values[2].ctls, &value64, &len, NULL, 0);
+        printf(RED"%-10s: "NOR"%.f MB\n", values[2].names, value64/1e+06);
+    } else {
+        sysctlbyname(values[i].ctls, NULL, &len, NULL, 0);
+        char *type = malloc(len);
+        sysctlbyname(values[i].ctls, type, &len, NULL, 0);
         printf(RED"%-10s: "NOR"%s\n", values[i].names, type);
+        free(type);
     }
 }
-void pkg(void) {
+static void envs(int i) {
+    char *type;
+    struct passwd *passwd;
+    if(i == 1) {
+        type = getenv("TERM");
+        printf(RED"Terminal  : "NOR"%s\n", type);
+    } else if(i ==2) {
+        passwd = getpwuid(getuid());
+        printf(RED"Shell     : "NOR"%s\n",passwd->pw_shell);
+    } else if(i == 3) {
+        passwd = getpwuid(getuid());
+        printf(RED"User      : "NOR"%s\n",passwd->pw_name);
+    }
+}
+static void pkg(void) {
     sqlite3 *db;
     int pkgs;
     sqlite3_stmt *s;
@@ -70,7 +116,7 @@ void pkg(void) {
     sqlite3_close(db);
     printf(RED"Packages  :"NOR" %d\n", pkgs);
 }
-void disk(void) {
+static void disk(void) {
     struct statvfs info;
     if (-1 == statvfs("/", &info))
         printf("failed to get disk info");
@@ -108,7 +154,7 @@ static void print_uptime(time_t *nowp)
         secs = uptime % 60;
         printf(RED"Uptime    : "NOR);
         if (days > 0)
-            (void)printf("%d day%s", days, days > 1 ? "s" : "");
+            (void)printf("%d day %s", days, days > 1 ? "s" : "");
         if (hrs > 0 && mins > 0)
             (void)printf("%2d:%02d", hrs, mins);
         else if (hrs > 0)
@@ -120,12 +166,36 @@ static void print_uptime(time_t *nowp)
         printf("\n");
     }
 }
-int main(void) {
-    time_t now;
-    (void)time(&now);
-    sysctls();
-    envs();
-    disk();
-    pkg();
-    print_uptime(&now);
+int main(int argc, char **argv) {
+    char c;
+    if (argc >= 2) {
+        while((c = getopt(argc, argv, "ha")) != -1) {
+            switch(c) {
+                case 'h':
+                    help();
+                    break;
+                case 'a':
+                    print_with_apple = true;
+                    break;
+                default:
+                    help();
+                    break;
+            }
+        }
+    }
+    if(print_with_apple) {
+        print_apple();
+    } else {
+        time_t now;
+        (void)time(&now);
+        for (int i=5;i>-1;i--) {
+            sysctls(i);
+        }
+        envs(1);
+        envs(2);
+        envs(3);
+        disk();
+        pkg();
+        print_uptime(&now);
+    }
 }
